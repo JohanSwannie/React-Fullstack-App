9f0037a3b62686bf2365e02c55ea82c50a9cb539
Johan Swan <jswannieman@gmail.com>
2023-11-04 12:45:08 +1300

    Commit vite.config JS Component

--- shopping-frontend/vite.config.js
+++ shopping-frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dns from "dns";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "localhost",
+    open: true,
+    port: 3000,
+  },
+});
