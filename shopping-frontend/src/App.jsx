4ff403c5709326a457460e22c43538844ac5e33c
Johan Swan <jswannieman@gmail.com>
2023-11-04 12:56:09 +1300

    Update App JSX Component

--- shopping-frontend/src/App.jsx
+++ shopping-frontend/src/App.jsx
@@ -1,10 +1,49 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { UserContextProvider } from "../context/userContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import Shopping from "./pages/Shopping";
+import Dashboard from "./pages/Dashboard";
+import axios from "axios";
+import { Toaster } from "react-hot-toast";
+
+axios.defaults.baseURL = "http://localhost:8000";
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <>
-      <div> HALLO </div>
-    </>
+    <UserContextProvider>
+      <Navbar />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          duration: 3500,
+          style: {
+            border: "5px solid #FFF",
+            borderStyle: "double",
+            padding: "16px",
+            backgroundColor: "#303030",
+            color: "#FFF",
+          },
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/shopping" element={<Shopping />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </UserContextProvider>
   );
 }
 
