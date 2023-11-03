02f098977f5ce1d6192f7d6152012fd8d0567277
Johan Swan <jswannieman@gmail.com>
2023-11-04 12:59:22 +1300

    Update Logout JSX Component

---
+++ shopping-frontend/src/pages/Logout.jsx
@@ -0,0 +1,54 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+export default function Logout() {
+  const { setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    axios
+      .get("/logout")
+      .then(() => {
+        toast.success("You have logged out successfully");
+        localStorage.setItem("session", "login");
+        localStorage.setItem("register", "notyet");
+        localStorage.removeItem("currentUser");
+        document.getElementById("register").style.display = "block";
+        document.getElementById("login").style.display = "block";
+        document.getElementById("logout").style.display = "none";
+        setUser(null);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const backToDashboard = () => {
+    navigate("/dashboard");
+  };
+
+  return (
+    <div className="logout">
+      <h2 className="subheader">Are you sure you want to log out</h2>
+      <button
+        id="logoutButton"
+        onClick={() => {
+          logoutUser();
+        }}
+      >
+        Yes, please log me out
+      </button>
+      <button
+        onClick={() => {
+          backToDashboard();
+        }}
+      >
+        No, I want to continue browsing
+      </button>
+    </div>
+  );
+}
