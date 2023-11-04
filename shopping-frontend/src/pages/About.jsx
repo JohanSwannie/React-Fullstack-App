8e8aa2e6f0b232d025c94feca3076142d77e6d4d
Johan Swan <jswannieman@gmail.com>
2023-11-04 16:29:03 +1300

    Update About JSX Component

--- shopping-frontend/src/pages/About.jsx
+++ shopping-frontend/src/pages/About.jsx
@@ -4,19 +4,21 @@ const About = () => {
   return (
     <div className="about">
       <h1 className="subheader">More About Us</h1>
-      <p className="about-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-        adipisci vero, praesentium molestiae vel saepe libero non natus earum
-        tempore eligendi, optio deserunt ipsam commodi incidunt veniam nihil
-        asperiores et minima illum fugiat expedita eos aspernatur. Esse velit
-        optio nihil animi eligendi quibusdam dolores recusandae at repudiandae
-        consequatur quia tempora aperiam ut dignissimos modi sint incidunt,
-        aliquam ratione temporibus architecto quae officia molestias nulla
-        provident. Culpa id deserunt sint quas nihil dolorum vitae voluptates
-        veritatis, ab perspiciatis, numquam expedita itaque unde? Eligendi vitae
-        explicabo ad velit vel quod, eius totam accusamus error ducimus a sed
-        delectus iste voluptates iure saepe.
-      </p>
+      <div className="about_container">
+        <p className="about-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          adipisci vero, praesentium molestiae vel saepe libero non natus earum
+          tempore eligendi, optio deserunt ipsam commodi incidunt veniam nihil
+          asperiores et minima illum fugiat expedita eos aspernatur. Esse velit
+          optio nihil animi eligendi quibusdam dolores recusandae at repudiandae
+          consequatur quia tempora aperiam ut dignissimos modi sint incidunt,
+          aliquam ratione temporibus architecto quae officia molestias nulla
+          provident. Culpa id deserunt sint quas nihil dolorum vitae voluptates
+          veritatis, ab perspiciatis, numquam expedita itaque unde? Eligendi
+          vitae explicabo ad velit vel quod, eius totam accusamus error ducimus
+          a sed delectus iste voluptates iure saepe.
+        </p>
+      </div>
     </div>
   );
 };
