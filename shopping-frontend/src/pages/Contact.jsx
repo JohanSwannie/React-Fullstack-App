76eb14bf4fdb869f1c4dd2278544661e256707d3
Johan Swan <jswannieman@gmail.com>
2023-11-04 16:29:17 +1300

    Update Contact JSX Component

--- shopping-frontend/src/pages/Contact.jsx
+++ shopping-frontend/src/pages/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
         {user ? `Hi ${user.name} - Feel free to contact us` : "Contact"}
       </h1>
       <div>
-        <h4>Email: whitiangacoastalboats@yahoo.co.nz</h4>
-        <h4>Telephone: +64 21 0334 4999</h4>
+        <p>Email: whitiangacoastalboats@yahoo.co.nz</p>
+        <p>Telephone: +64 21 0334 4999</p>
       </div>
     </div>
   );
