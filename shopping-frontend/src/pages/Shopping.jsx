68a3512d4fae9136cd6dee1fe859ec7b163db145
Johan Swan <jswannieman@gmail.com>
2023-11-05 18:04:53 +1300

    Update Shopping JSX Component

--- shopping-frontend/src/pages/Shopping.jsx
+++ shopping-frontend/src/pages/Shopping.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Row, Col } from "react-bootstrap";
+import { MusicCategories } from "../components/MusicCategories";
+import itemCategories from "../data/itemCategories.json";
 
-const Shopping = () => {
-  return <div>Shopping</div>;
-};
+function Shopping() {
+  window.scrollTo(0, 0);
+
+  return (
+    <>
+      <h2>Swannie's Shopping Categories</h2>
+      <Row lg={3} md={2} sm={1} xs={1} className="g-3">
+        {itemCategories.map((item) => (
+          <Col key={item.categoryId}>
+            <MusicCategories {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+}
 
 export default Shopping;
